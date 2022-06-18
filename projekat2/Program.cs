f954a81321524ab61a68513154eb1dd13b3e246a
Djordje V <107632319+djjordjev@users.noreply.github.com>
2022-06-18 19:47:45 +0200

    Update Program.cs
    Generating random mines, output etc.

--- projekat2/Program.cs
+++ projekat2/Program.cs
@@ -344,6 +344,310 @@ Pritinsite [Enter] da nastavite...");
             ponavljaj = false;
         }
 
+        //upis mina nasumicno u tabelu
+        static Polje[,] Upisi_minice(Polje[,] tabelica, int mine, int x_dimenzije, int y_dimenzije)
+        {
+            Random rand_generator = new Random();
+            for (int i = 0; i < mine; i++)
+            {
+                int minica_x = rand_generator.Next(0, x_dimenzije);
+                int minica_y = rand_generator.Next(0, y_dimenzije);
+                if (tabelica[minica_x, minica_y].mina)
+                {
+                    i--;
+                }
+                else tabelica[minica_x, minica_y].mina = true;
+            }
+            return tabelica;
+        }
+        //metoda za ispis tabele nakon 
+        static void Ispis_Tabelice_Poraz(Polje[,] tabelica)
+        {
+            Naslov();
+            Console.WriteLine(Uputstva);
+            Console.WriteLine();
+            for (int i = 0; i < tabelica.GetLength(0); i++)
+            {
+                Console.Write("  ");
+                for (int j = 0; j < tabelica.GetLength(1); j++)
+                {
+                    if (tabelica[i, j].mina != true)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
+                        Console.Write(Izbroji_Susede(tabelica, i, j));
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("☼");
+                        Console.ResetColor();
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pritisnite [Enter] da nastavite...");
+            ponavljaj = false;
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter)
+            { }
+        }
+
+        static int[] BiranjePolja(Polje[,] tabela, int trenutno_x, int trenutno_y, int mine)
+        {
+            int brojNeotvorenihPolja = 0;
+            for (int j = 0; j < tabela.GetLength(0); j++)
+            {
+                for (int k = 0; k < tabela.GetLength(1); k++)
+                {
+                    if (!tabela[j, k].poseceno)
+                    {
+                        brojNeotvorenihPolja++;
+                    }
+                }
+            }
+
+            int[] x_i_y = new int[2];
+
+            if (brojNeotvorenihPolja == mine)
+            {
+                Surenje();
+            }
+
+            ConsoleKeyInfo pritisnuto_dugme;
+            do
+            {
+                pritisnuto_dugme = Console.ReadKey(true);
+
+                if (pritisnuto_dugme.Key == ConsoleKey.UpArrow)
+                {
+                    if (trenutno_x == 0)
+                    {
+                        trenutno_x = tabela.GetLength(0) - 1;
+                    }
+                    else
+                    {
+                        trenutno_x--;
+                    }
+                    Console.Clear();
+                    Ispis(tabela, trenutno_x, trenutno_y, mine);
+                }
+                else if (pritisnuto_dugme.Key == ConsoleKey.DownArrow)
+                {
+                    if (trenutno_x == tabela.GetLength(0) - 1)
+                    {
+                        trenutno_x = 0;
+                    }
+                    else
+                    {
+                        trenutno_x++;
+                    }
+                    Console.Clear();
+                    Ispis(tabela, trenutno_x, trenutno_y, mine);
+                }
+                else if (pritisnuto_dugme.Key == ConsoleKey.RightArrow)
+                {
+                    if (trenutno_y == tabela.GetLength(1) - 1)
+                    {
+                        trenutno_y = 0;
+                    }
+                    else
+                    {
+                        trenutno_y++;
+                    }
+                    Console.Clear();
+                    Ispis(tabela, trenutno_x, trenutno_y, mine);
+                }
+                else if (pritisnuto_dugme.Key == ConsoleKey.LeftArrow)
+                {
+                    if (trenutno_y == 0)
+                    {
+                        trenutno_y = tabela.GetLength(1) - 1;
+                    }
+                    else
+                    {
+                        trenutno_y--;
+                    }
+                    Console.Clear();
+                    Ispis(tabela, trenutno_x, trenutno_y, mine);
+                }
+                else if (pritisnuto_dugme.Key == ConsoleKey.F)
+                {
+                    if (!tabela[trenutno_x, trenutno_y].upitnik && !tabela[trenutno_x, trenutno_y].zastavica && !tabela[trenutno_x, trenutno_y].poseceno)
+                    {
+                        tabela[trenutno_x, trenutno_y].zastavica = true;
+                    }
+                    else
+                    {
+                        tabela[trenutno_x, trenutno_y].zastavica = false;
+                    }
+                    Console.Clear();
+                    Ispis(tabela, trenutno_x, trenutno_y, mine);
+                }
+                else if (pritisnuto_dugme.Key == ConsoleKey.U)
+                {
+                    if (!tabela[trenutno_x, trenutno_y].zastavica && !tabela[trenutno_x, trenutno_y].upitnik && !tabela[trenutno_x, trenutno_y].poseceno)
+                    {
+                        tabela[trenutno_x, trenutno_y].upitnik = true;
+                    }
+                    else
+                    {
+                        tabela[trenutno_x, trenutno_y].upitnik = false;
+                    }
+                    Console.Clear();
+                    Ispis(tabela, trenutno_x, trenutno_y, mine);
+                }
+                else if (pritisnuto_dugme.Key == ConsoleKey.Escape)
+                {
+                    Console.Clear();
+                    Ispis_Tabelice_Poraz(tabela);
+                    break;
+                }
+            }
+            while (pritisnuto_dugme.Key != ConsoleKey.Enter);
+            x_i_y[0] = trenutno_x;
+            x_i_y[1] = trenutno_y;
+            return x_i_y;
+        }
+        //ispis tabele
+        static void Ispis(Polje[,] tabela, int trenutni_x, int trenutni_y, int mine)
+        {
+            Naslov();
+            Console.WriteLine(Uputstva);
+            Console.WriteLine();
+            for (int i = 0; i < tabela.GetLength(0); i++)
+            {
+                Console.Write("  ");
+                for (int j = 0; j < tabela.GetLength(1); j++)
+                {
+                    if (tabela[i, j].zastavica)
+                    {
+                        if (i == trenutni_x && j == trenutni_y)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Gray;
+                        }
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.Write("!");
+                    }
+                    else if (tabela[i, j].upitnik)
+                    {
+                        if (i == trenutni_x && j == trenutni_y)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Gray;
+                        }
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.Write("?");
+                    }
+                    else if (tabela[i, j].poseceno)
+                    {
+                        if (i == trenutni_x && j == trenutni_y)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Gray;
+                        }
+                        int susedi = Izbroji_Susede(tabela, i, j);
+                        if (susedi == 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write(susedi);
+                        }
+                        else if (susedi == 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(susedi);
+                        }
+                        else if (susedi == 3)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.Write(susedi);
+                        }
+                        else if (susedi == 4)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkBlue;
+                            Console.Write(susedi);
+                        }
+                        else if (susedi == 5)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            Console.Write(susedi);
+                        }
+                        else if (susedi == 6)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            Console.Write(susedi);
+                        }
+                        else if (susedi == 7)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                            Console.Write(susedi);
+                        }
+                        else if (susedi == 8)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.Write(susedi);
+                        }
+                        else if (susedi == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            Console.Write("-");
+                        }
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        if (i == trenutni_x && j == trenutni_y)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Gray;
+                        }
+                        Console.Write(" ");
+                    }
+                    Console.ResetColor();
+                }
+
+                int brZastavica = 0;
+
+                for (int j = 0; j < tabela.GetLength(0); j++)
+                {
+                    for (int k = 0; k < tabela.GetLength(1); k++)
+                    {
+                        if (tabela[j, k].zastavica)
+                        {
+                            brZastavica++;
+                        }
+                    }
+                }
+                int maxm = mine;
+                if (i == tabela.GetLength(0) - 1)
+                {
+                    Console.Write("\n\nPreostale zastave: ");
+                    if (maxm - brZastavica < 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                    }
+                    Console.WriteLine(maxm - brZastavica);
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
